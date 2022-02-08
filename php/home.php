eaeade88e8abfbb9912bef9717ff82e065efe913
vincentvanslagmolen@gmail.com <vincentvanslagmolen@gmail.com>
2022-02-08 17:40:20 +0100

    Content on Homepage, edit to css & added image for homepage

--- php/home.php
+++ php/home.php
@@ -8,11 +8,57 @@ PrintHead();
 PrintNavbar();
 ?>
 
-<div class="container">
-    <div class="row">
-
-    </div>
+<div class="d-flex justify-content-center" style="margin-top: -2rem">
+    <img class="w-100" src="img/intro.png">
 </div>
 
+
+<section class="my-5">
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h1 class="text-white mb-5 d-inline-block">Latest News</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-4 mb-4">
+                <div class="article">
+                    <p>07 Feb 2022 | <span>NEWS</span></p>
+                    <h2>Martinot-Lagarde seeks more medal success in Belgrade</h2>
+                </div>
+            </div>
+            <div class="col-lg-4 mb-4">
+                <div class="article">
+                    <p>07 Feb 2022 | <span>NEWS</span></p>
+                    <h2>Vacancy: Health and Science internship</h2>
+                </div>
+            </div>
+            <div class="col-lg-4 mb-4">
+                <div class="article">
+                    <p>06  Feb 2022 | <span>NEWS</span></p>
+                    <h2>Mechaal shines with record run in Staten Island</h2>
+                </div>
+            </div>
+            <div class="col-lg-4 mb-4">
+                <div class="article">
+                    <p>07 Feb 2022 | <span>NEWS</span></p>
+                    <h2>World champion Lyles targets fast finish to season in Birmingham</h2>
+                </div>
+            </div>
+            <div class="col-lg-4 mb-4">
+                <div class="article">
+                    <p>08 Feb 2022 | <span>NEWS</span></p>
+                    <h2>Thompson-Herah takes to  the track in Torun</h2>
+                </div>
+            </div>
+            <div class="col-lg-4 mb-4">
+                <div class="article">
+                    <p>08 Feb 2022 | <span>NEWS</span></p>
+                    <h2>Five athletes on British team for World Race Walking Team Championships in Muscat</h2>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 </body>
 </html>
