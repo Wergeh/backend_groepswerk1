8df979f20845ac0ab6515efe3bf8f974061dd3d6
Stijn Gyssens <stijn.gyssens@gmail.com>
2022-02-10 01:20:01 +0100

    testing in admin.php

--- php/admin.php
+++ php/admin.php
@@ -1,2 +1,26 @@
 <?php
 require_once "lib/autoload.php";
+$query = 'select concat_ws(" ",first_name, last_name) as Athlete
+                    from atleet';
+
+$data = GetData($query);
+
+if (key_exists("Athlete",$_GET)){
+    $query2='select atleetID, concat_ws(" ",first_name, last_name) as Athlete
+from atleet
+having Athlete like "'.$_GET["Athlete"].'"';
+    $data2=GetData($query2);
+    var_dump($data2);
+}
+
+$html= "
+    <form method='GET'>
+    <input list='Athlete' name='Athlete'>
+    <datalist id='Athlete'>
+    ";
+foreach ($data as $row){
+    $html.="<option value='{$row["Athlete"]}'>";
+}
+$html.= "</datalist> <input type='submit'></form>";
+
+print $html;
