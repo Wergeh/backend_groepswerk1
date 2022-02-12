31c98536136e3030013ff22953b4dd83909b5783
StijnGyssens <stijn.gyssens@gmail.com>
2022-02-12 12:24:46 +0100

    add delete record

--- php/lib/delete.php
+++ php/lib/delete.php
@@ -1,1 +1,8 @@
 <?php
+
+require_once "autoload.php";
+
+$query="delete from record where recordID ={$_GET['recordID']}";
+ExecuteSQL($query);
+var_dump($query);
+header("Location:../world_records.php");
